f33424ef7e2b817253791c3bbeacf3f9dedf4be4
Zarif Atique <zarifatique02@gmail.com>
2025-07-09 04:03:48 +1000

    Initialize linked list data structure

---
+++ linkedlist.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node Node;
+
+struct node {
+    int data;
+    Node* next;
+};
+
+Node* list_init(int value) {
+    Node* head = malloc(sizeof(Node));
+    if (head == NULL) {
+        printf("Memory allocation failed.\n");
+        return NULL;
+    }
+    head->data = value;
+    head->next = NULL;
+    
+    return head;
+}
+
+void list_add(struct node** head, int value) {
+    Node* new_node = malloc(sizeof(Node));
+    if (new_node == NULL) {
+        printf("Memory allocation failed.\n");
+        return;
+    }
+    new_node->data = value;
+    new_node->next = NULL;
+
+    if (*head == NULL) {
+        *head = new_node;
+    } else {
+        Node* temp = *head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
+}
+
