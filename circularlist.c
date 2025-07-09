0ea8e4e8c98d54f7bdc777046bcf7efde95c6f56
Zarif Atique <zarifatique02@gmail.com>
2025-07-09 13:41:34 +1000

    Final circular list

--- circularlist.c
+++ circularlist.c
@@ -37,10 +37,47 @@ void list_add(node** head, int value) {
         new_node->prev = new_node;
     } else {
         node* tail = (*head)->prev;
-        
+
         tail->next = new_node;
         new_node->prev = tail;
         new_node->next = *head;
         (*head)->prev = new_node;
     }
+}
+
+void list_remove(node** head, node* n) {
+    if (head == NULL || *head == NULL || n == NULL) {
+        return;
+    }
+
+    if (*head == n) {
+        if (n->next == n) {
+            free(n);
+            *head = NULL;
+        } else {
+            node* tail = n->prev;
+            
+            tail->next = n->next;
+            n->next->prev = tail;
+            *head = n->next;
+            free(n);
+        }
+    } else {
+        n->next->prev = n->prev;
+        n->prev->next = n->next;
+        free(n);
+    }
+}
+
+void list_free(node** head) {
+    if (head == NULL || *head == NULL) return;
+
+    node* current = *head;
+    node* next;
+    do {
+        next = current->next;
+        free(current);
+        current = next;
+    } while (current != *head);
+    *head = NULL;
 }
