21a76a27911dba967d8937dc8c31e77b633ea2b7
Zarif Atique <zarifatique02@gmail.com>
2025-07-22 15:21:25 +1000

    Implement dequeue function

--- Priority Queue(List based)/listqueue.c
+++ Priority Queue(List based)/listqueue.c
@@ -22,3 +22,38 @@ bool queue_front(ListQueue* q, int* result) {
     return true;
 }
 
+void queue_enqueue(ListQueue* q, int value) {
+    if (q == NULL) return;
+
+    Node* new_node = malloc(sizeof(Node));
+    if (new_node == NULL) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        return;
+    }
+    new_node->value = value;
+    new_node->next = NULL;
+
+    if (q->front == NULL) {
+        q->front = new_node;
+        q->back = new_node;
+    } else {
+        q->back->next = new_node;
+        q->back = new_node;
+    }
+}
+
+bool queue_dequeue(ListQueue* q, int* result) {
+    if (q == NULL || q->front == NULL || result == NULL) return false;
+
+    *result = q->front->value;
+    
+    Node* temp = q->front;
+    if (q->front == q->back) {
+        q->front = NULL;
+        q->back = NULL;
+    } else {
+        q->front = q->front->next;
+    }
+    free(temp);
+    return true;
+}
